866593ba5157289ff7fbd545307d3d28da0d45c9
brunellanmcarvalho1 <brunella.nmcarvalho1@gmail.com>
2025-02-01 15:04:21 +0100

    add Run Images

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import AddRunningPage from "./pages/AddRunningPage/AddRunningPage";
+import AddRunningPage from "./pages/AddRunningPage/AddRunningPage";
 import HomePage from "./pages/HomePage/HomePage";
 import Sidebar from "./components/Sidebar/Sidebar";
-import Navbar from "./components/Navbar/Navbar"
+import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import About from "./pages/AboutPage/About";
 import "./App.css";
@@ -12,12 +12,12 @@ function App() {
   return (
     <Router>
       <div className="app-container">
-      <Navbar /> 
+        <Navbar />
         <div className="main-content">
           <Sidebar />
           <Routes>
             <Route path="/" element={<HomePage />} />
-            {/* <Route path="/add-running" element={<AddRunningPage />} /> */}
+            <Route path="/add-running" element={<AddRunningPage />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </div>
