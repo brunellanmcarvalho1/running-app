d1428573ec91712222aa014c3b2b1ae0ac071bef
brunellanmcarvalho1 <brunella.nmcarvalho1@gmail.com>
2025-02-07 22:54:26 +0100

    Add calendar page

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,29 @@ import Footer from "./components/Footer/Footer";
 import About from "./pages/AboutPage/About";
 import TrainingDetails from "./pages/Trainingdetails/TrainingDetails";
 import TrainingLogPage from "./pages/TrainingLogPage/TrainingLogPage";
+import CalendarPage from "./pages/CalendarPage/CalendarPage";
 import "./App.css";
 
 function App() {
   const [isSidebarExpanded, setIsSidebarExpanded] = useState(false);
+  const [trainings, setTrainings] = useState([]);
+
+  const handleAddTraining = (newTraining) => {
+    setTrainings([...trainings, newTraining]);
+  };
 
   return (
     <Router>
       <div className="flex min-h-screen">
-        <Sidebar isExpanded={isSidebarExpanded} toggleSidebar={() => setIsSidebarExpanded(!isSidebarExpanded)} />
-        <div className={`flex-1 transition-all duration-300 ${isSidebarExpanded ? "ml-64" : "ml-16"}`}>
+        <Sidebar
+          isExpanded={isSidebarExpanded}
+          toggleSidebar={() => setIsSidebarExpanded(!isSidebarExpanded)}
+        />
+        <div
+          className={`flex-1 transition-all duration-300 ${
+            isSidebarExpanded ? "ml-64" : "ml-16"
+          }`}
+        >
           <Navbar />
           <div className="p-4">
             <Routes>
@@ -25,7 +38,14 @@ function App() {
               <Route path="/training-log" element={<TrainingLogPage />} />
               <Route path="/add-running" element={<AddRunningPage />} />
               <Route path="/about" element={<About />} />
-              <Route path="/training/:trainingId" element={<TrainingDetails />} />
+              <Route
+                path="/training/:trainingId"
+                element={<TrainingDetails />}
+              />
+              <Route
+                path="/calendar"
+                element={<CalendarPage trainings={trainings} />}
+              />
             </Routes>
           </div>
           <Footer />
