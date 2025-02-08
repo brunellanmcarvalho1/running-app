284a34385b5abb79a4dfa613bd53a9d966c9185c
vicmarchesi <victor.marchesi91@gmail.com>
2025-02-08 14:30:39 +0000

    Styling

--- src/components/TrainingCard/TrainingCard.jsx
+++ src/components/TrainingCard/TrainingCard.jsx
@@ -1,6 +1,10 @@
 import PropTypes from "prop-types";
 import { Pencil, Trash2 } from "lucide-react";
 import { Link } from "react-router-dom";
+import streetRunImage from "../../assets/streetRun.jpg";
+import trailRunImage from "../../assets/trailRun.jpg";
+import trackRunImage from "../../assets/trackRun.jpg";
+import treadmillRunImage from "../../assets/treadmillRun.jpg";
 
 const formatDate = (dateString) => {
   const [year, month, day] = dateString.split("-");
@@ -11,54 +15,66 @@ const formatType = (type) => {
   return type.replace(/([a-z])([A-Z])/g, "$1 $2");
 };
 
+const getRunTypeImage = (runType) => {
+  switch (runType) {
+    case "streetRun":
+      return streetRunImage;
+    case "trailRun":
+      return trailRunImage;
+    case "trackRun":
+      return trackRunImage;
+    case "treadmillRun":
+      return treadmillRunImage;
+    default:
+      return "";
+  }
+};
+
 const typeLabels = {
   longRun: "Long Run",
   intervalRun: "Interval Run",
   recoveryRun: "Recovery Run",
-  tempoRun: "tempoRun",
+  tempoRun: "Tempo Run",
   streetRun: "Street",
   trailRun: "Trail",
   trackRun: "Track",
   treadmillRun: "Treadmill"
 };
-
 const getTypeLabel = (type) => typeLabels[type] || formatType(type);
 
-
 const TrainingCard = ({ run, onEdit, onDelete }) => {
+  const runTypeImage = getRunTypeImage(run.runType);
+
   return (
     <Link to={`/training/${run.id}`} className="training-card-link">
-    <div className="relative bg-white rounded-2xl shadow-lg p-6 text-center hover:shadow-xl transform hover:-translate-y-1 transition-transform duration-200 transition-all mb-8">
-      {run.runTypeImage && (
-        <img
-          src={run.runTypeImage}
-          alt={formatType(run.runType)}
-          className="w-full max-w-sm mx-auto rounded-lg mb-4" 
-        />
-      )}
-      <h3 className="text-xl font-semibold mt-4 text-gray-900">{formatDate(run.date)}</h3>
-      <p className="text-gray-700 font-medium">{getTypeLabel(run.runType)}</p>
+      <div className="relative bg-white rounded-2xl shadow-lg p-6 text-center hover:shadow-xl transform hover:-translate-y-1 transition-transform duration-200 transition-all mb-8">
+        {runTypeImage && (
+          <img
+            src={runTypeImage}
+            alt={formatType(run.runType)}
+            className="w-full max-w-xs mx-auto rounded-lg mb-4"
+          />
+        )}
+        <h3 className="text-xl font-semibold mt-4 text-gray-900">
+          {formatDate(run.date)}
+        </h3>
+        <p className="text-gray-700 font-medium">{getTypeLabel(run.runType)}</p>
 <p className="text-gray-700 font-medium">{getTypeLabel(run.trainingType)}</p>
-
-      <div className="flex justify-center space-x-4 mt-4"> 
-       
-        <button
-          onClick={() => onEdit(run)}
-          className="px-4 py-2 bg-cyan-500 hover:bg-cyan-600 text-white font-semibold rounded-lg shadow-md transition flex items-center space-x-2"
-        >
-          <Pencil size={20} /> 
-          
-        </button>
-      
-        <button
-          onClick={() => onDelete(run.id)}
-          className="px-4 py-2 bg-pink-500 hover:bg-pink-600 text-white font-semibold rounded-lg shadow-md transition flex items-center space-x-2"
-        >
-          <Trash2 size={20} /> 
-          
-        </button>
+        <div className="flex justify-center space-x-4 mt-4">
+          <button
+            onClick={() => onEdit(run)}
+            className="px-4 py-2 bg-cyan-500 hover:bg-cyan-600 text-white font-semibold rounded-lg shadow-md transition flex items-center space-x-2"
+          >
+            <Pencil size={20} />
+          </button>
+          <button
+            onClick={() => onDelete(run.id)}
+            className="px-4 py-2 bg-pink-500 hover:bg-pink-600 text-white font-semibold rounded-lg shadow-md transition flex items-center space-x-2"
+          >
+            <Trash2 size={20} />
+          </button>
+        </div>
       </div>
-    </div>
     </Link>
   );
 };
@@ -69,7 +85,6 @@ TrainingCard.propTypes = {
     date: PropTypes.string.isRequired,
     trainingType: PropTypes.string.isRequired,
     runType: PropTypes.string.isRequired,
-    runTypeImage: PropTypes.string,
   }).isRequired,
   onEdit: PropTypes.func.isRequired,
   onDelete: PropTypes.func.isRequired,
